f18451568fb74490bb5af51e6a3dcd897cf92666
DmitryP0l <dmitrypoletaeu@gmail.com>
2022-06-11 12:45:35 +0300

    commit

---
+++ MusicByApple/MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarController.swift
+//  MusicByApple
+//
+//  Created by lion on 10.06.22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setControllers()        
+    }
+    
+    private func setControllers() {
+        viewControllers = [ generateViewController(rootVC: SearchViewController(),
+                                                   image: UIImage(systemName: "magnifyingglass")!, title: "Search"),
+                            generateViewController(rootVC: ViewController(),
+                                                   image: UIImage(systemName: "music.note.list")!, title: "Library")
+        ]
+    }
+    
+    private func generateViewController(rootVC: UIViewController, image: UIImage, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootVC)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootVC.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        tabBar.tintColor = .systemPink
+        return navigationVC
+    }
+    
+}
---
+++ MusicByApple/SearchViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SearchViewController.swift
+//  MusicByApple
+//
+//  Created by lion on 10.06.22.
+//
+
+import UIKit
+
+struct TrackModel{
+    var trackName: String
+    var artistName: String
+}
+
+class SearchViewController: UITableViewController {
+    
+    let tracks: [TrackModel] = [TrackModel(trackName: "qq", artistName: "df"),
+                               TrackModel(trackName: "JJH", artistName: "JHHG")]
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = UIImage(systemName: "square.grid.3x1.below.line.grid.1x2")
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
