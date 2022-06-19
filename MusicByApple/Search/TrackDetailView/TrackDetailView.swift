54ab92558b9eb8087fed6c3dcc1ea23e8a53fa10
DmitryP0l <dmitrypoletaeu@gmail.com>
2022-06-19 12:38:14 +0300

    commit
    Scale animations and time observer

--- MusicByApple/Search/TrackDetailView/TrackDetailView.swift
+++ MusicByApple/Search/TrackDetailView/TrackDetailView.swift
@@ -28,8 +28,69 @@ final class TrackDetailView: UIView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        trackImageView.backgroundColor = .systemCyan
+        setTrackImageView()
     }
+    
+//MARK: - Setup
+    
+    private func setTrackImageView() {
+        trackImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        trackImageView.layer.cornerRadius = 8
+    }
+    
+    func set(viewModel: SearchViewModel.Cell) {
+        trackTitleLabel.text = viewModel.trackName
+        trackAuthorLAbel.text = viewModel.artistName
+        
+        playTrack(previewUrl: viewModel.previewUrl)
+        monitorStartTime()
+        
+        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
+        guard let url = URL(string: string600 ?? "") else { return }
+        trackImageView.sd_setImage(with: url, completed: nil)
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        
+        guard let previewUrl = previewUrl, let url = URL(string: previewUrl) else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    
+//MARK: - Time setup
+    
+    private func monitorStartTime() {
+        let time = CMTimeMake(value: 1, timescale: 3)
+        let times = [NSValue(time: time)]
+        player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
+            self?.enlagreTrackImageView()
+        }
+    }
+    
+//MARK: - Animations
+    
+    private func enlagreTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.trackImageView.transform = .identity
+            self.trackImageView.layer.cornerRadius = 8
+        } completion: { _ in }
+
+    }
+    
+    private func reduceTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.setTrackImageView()
+        } completion: { _ in }
+
+    }
+    
+}
+
+
+//MARK: - @IBActions
+
+extension TrackDetailView {
     
     @IBAction func dragDownButtonTapped(_ sender: UIButton) {
         self.removeFromSuperview()
@@ -44,9 +105,11 @@ final class TrackDetailView: UIView {
         if player.timeControlStatus == .paused {
             player.play()
             playPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
+            enlagreTrackImageView()
         } else {
             player.pause()
             playPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+            reduceTrackImageView()
         }
     }
     
@@ -55,26 +118,4 @@ final class TrackDetailView: UIView {
     
     @IBAction func handleVolumeSlider(_ sender: UISlider) {
     }
-    
-    func set(viewModel: SearchViewModel.Cell) {
-        trackTitleLabel.text = viewModel.trackName
-        trackAuthorLAbel.text = viewModel.artistName
-        
-        playTrack(previewUrl: viewModel.previewUrl)
-        
-        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
-        guard let url = URL(string: string600 ?? "") else { return }
-        trackImageView.sd_setImage(with: url, completed: nil)
-    }
-    
-    private func playTrack(previewUrl: String?) {
-        print("playing \(previewUrl)")
-        guard let previewUrl = previewUrl, let url = URL(string: previewUrl) else { return }
-        let playerItem = AVPlayerItem(url: url)
-        player.replaceCurrentItem(with: playerItem)
-        player.play()
-        
-
-    }
-    
 }
